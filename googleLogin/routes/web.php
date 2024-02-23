8c5fb3b3090d7c0c44ec31f18cb3b3cb5da60830
Jacob Holtz <jholtz1@hawk.iit.edu>
2024-02-22 22:35:18 -0600

    uploaded googleLogin

---
+++ googleLogin/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GoogleLoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(GoogleLoginController::class)->group(function(){
+    Route::get('/google/redirect', 'redirectToGoogle')->name('google.redirect');
+    Route::get('/google/callback', 'handleGoogleCallback')->name('google.callback');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
